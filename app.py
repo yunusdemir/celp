2fb7322043b01d66515e20bd66a28314fc687ab5
jelleas <jelle.van.assema@gmail.com>
2019-05-02 11:29:14 +0200

    cleanup stars, put content in carousel

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import data
 from recommend import recommend
 
 from tempfile import mkdtemp
-from copy import deepcopy
 from flask import Flask, render_template, redirect, request, session, flash
 from flask_session import Session
 
@@ -18,7 +17,7 @@ def index():
     user = session.get("user")
     user_id = user["user_id"] if user else None
 
-    recommendations = deepcopy(recommend(user_id=user_id, n=10))
+    recommendations = recommend(user_id=user_id, n=10)
 
     return render_template("index.html", recommendations=recommendations, user=session.get("user"))
 
@@ -44,18 +43,14 @@ def business(city, id):
     user = session.get("user")
     user_id = user["user_id"] if user else None
 
-    business = deepcopy(data.get_business(city.lower(), id))
-    business["stars"] = int(float(business["stars"]) * 2)
+    business = data.get_business(city.lower(), id)
 
-    reviews = deepcopy(data.get_reviews(city=business["city"].lower(), business_id=business["business_id"]))
-    for review in reviews:
-        review["stars"] = int(float(review["stars"]) * 2)
+    reviews = data.get_reviews(city=business["city"].lower(), business_id=business["business_id"])
 
-    recommendations = deepcopy(recommend(user_id=user_id, business_id=id, city=business["city"].lower(), n=10))
+    recommendations = recommend(user_id=user_id, business_id=id, city=business["city"].lower(), n=10)
     for recommendation in recommendations:
         assert "business_id" in recommendation
         assert "stars" in recommendation
-        recommendation["stars"] = int(float(recommendation["stars"]) * 2)
         assert "name" in recommendation
         assert "city" in recommendation
         assert "address" in recommendation
