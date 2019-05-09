fd66049f22806e1a63347db657224da897c7c456
Yunus Demir <contact.github@yunusdemir.nl>
2019-05-09 17:15:12 +0200

    Recommender.py is also a class now

--- recommender.py
+++ recommender.py
@@ -1,21 +1,31 @@
-from data import CITIES, BUSINESSES, USERS, REVIEWS, TIPS, CHECKINS
-
 import random
 
+from data import Data
 
-def recommend(user_id=None, business_id=None, city=None, n=10):
-    """
-    Returns n recommendations as a list of dicts.
-    Optionally takes in a user_id, business_id and/or city.
-    A recommendation is a dictionary in the form of:
-        {
-            business_id:str
-            stars:str
-            name:str
-            city:str
-            adress:str
-        }
-    """
-    if not city:
-        city = random.choice(CITIES)
-    return random.sample(BUSINESSES[city], n)
+
+class Recommender:
+
+    def __init__(self, user_id=None, business_id=None, city=None, n=10):
+        self.user_id = user_id
+        self.business_id = business_id
+        self.city = city
+        self.n = n
+
+        self.data = Data()
+
+    def recommend(self):
+        """
+        Returns n recommendations as a list of dicts.
+        Optionally takes in a user_id, business_id and/or city.
+        A recommendation is a dictionary in the form of:
+            {
+                business_id:str
+                stars:str
+                name:str
+                city:str
+                adress:str
+            }
+        """
+        if not self.city:
+            self.city = random.choice(self.data.CITIES)
+        return random.sample(self.data.BUSINESSES[self.city], self.n)
