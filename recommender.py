aff82d2de2b8ebdee4205ab4be950ffc004447f3
Yunus Demir <contact.github@yunusdemir.nl>
2019-05-10 13:50:32 +0200

    Updated recommender to change variables

--- recommender.py
+++ recommender.py
@@ -5,15 +5,10 @@ from data import Data
 
 class Recommender:
 
-    def __init__(self, user_id=None, business_id=None, city=None, n=10):
-        self.user_id = user_id
-        self.business_id = business_id
-        self.city = city
-        self.n = n
-
+    def __init__(self):
         self.data = Data()
 
-    def recommend(self):
+    def recommend(self, user_id=None, business_id=None, city=None, n=10):
         """
         Returns n recommendations as a list of dicts.
         Optionally takes in a user_id, business_id and/or city.
@@ -26,6 +21,6 @@ class Recommender:
                 adress:str
             }
         """
-        if not self.city:
-            self.city = random.choice(self.data.CITIES)
-        return random.sample(self.data.BUSINESSES[self.city], self.n)
+        if not city:
+            city = random.choice(self.data.CITIES)
+        return random.sample(self.data.BUSINESSES[city], n)
