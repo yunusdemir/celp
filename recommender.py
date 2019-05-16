fd682dcddd2b27f880d824b4edbf8e20f6537da7
Yunus Demir <contact.github@yunusdemir.nl>
2019-05-16 13:56:27 +0200

    Added function to calculate similarity matrix by categories
    Signed-off-by: Yunus Demir <contact.github@yunusdemir.nl>

--- recommender.py
+++ recommender.py
@@ -1,12 +1,15 @@
 import random
 
+import numpy as np
+import pandas as pd
+
 from data import Data
 
 
 class Recommender:
 
-    def __init__(self):
-        self.data = Data()
+    def __init__(self, data_directory="data"):
+        self.data = Data(data_directory)
 
     def recommend(self, user_id=None, business_id=None, city=None, n=10):
         """
@@ -24,3 +27,18 @@ class Recommender:
         if not city:
             city = random.choice(self.data.CITIES)
         return random.sample(self.data.BUSINESSES[city], n)
+
+    def create_similarity_matrix_categories(self, df_categories: pd.DataFrame) -> pd.DataFrame:
+        """Create a similarity matrix for categories"""
+
+        df = self.data.extract_categories(df_categories)
+        df_utility_categories = self.data.pivot_categories(df)
+
+        npu = df_utility_categories.values
+        m1 = npu @ npu.T
+        m2 = m1 / np.diag(m1)
+        m3 = np.minimum(m2, m2.T)
+        return pd.DataFrame(m3, index=df_utility_categories.index,
+                            columns=df_utility_categories.index)
+
+    # def business_logged_in(self, user_id, business_id, city):
