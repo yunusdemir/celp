5de9595a524a4102c30f9005812bfc03b0ece15f
joostdw11 <joost-dewildt@hotmail.com>
2019-05-18 10:22:17 +0200

    Evaluate tweek

--- evaluate.py
+++ evaluate.py
@@ -1,26 +1,31 @@
+import random
+
 from recommender import Recommender
 
 rec = Recommender()
 
+class Evaluate:
 
-def mse(predicted_ratings):
-    """Computes the mean square error between actual ratings and predicted ratings
+    def mse(self, predicted_ratings):
+        """Computes the mean square error between actual ratings and predicted ratings
 
-    Arguments:
-    predicted_ratings -- a dataFrame containing the columns rating and predicted rating
-    """
-    diff = predicted_ratings['stars'] - predicted_ratings['predicted stars']
-    return (diff ** 2).mean()
-
-
-def item_based_filtered(user_id):
-    return rec.predict_rating(user_id, 4)
+        Arguments:
+        predicted_ratings -- a dataFrame containing the columns rating and predicted rating
+        """
+        diff = predicted_ratings['stars'] - predicted_ratings['predicted stars']
+        return (diff ** 2).mean()
 
 
-def mse_item_based():
-    for city in rec.data.CITIES:
-        mse_list = [mse(item_based_filtered(user["user_id"])) for user in rec.data.USERS[city]]
-        print(min(mse_list), max(mse_list), sum(mse_list)/len(sum_list))
+    def item_based_filtered(self, user_id):
+        return rec.predict_rating(user_id, 4)
 
 
-mse_item_based()
+    def mse_item_based(self, n):
+        mse_list = []
+        for _ in range(n):
+            city = random.choice(rec.data.CITIES)
+            user = random.choice(rec.data.USERS[city])
+            mse_list.append(self.mse(self.item_based_filtered(user["user_id"])))
+
+
+Evaluate().mse_item_based(3)
